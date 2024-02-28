82bee1403815e8978fdf464c8cba6f5d1a3026a9
LeleVaneck10 <leletokamvaneckauriol@gmail.com>
2024-02-28 22:53:02 +0100

    Created and Initialized the spring boot project

---
+++ src/main/java/com/training/ecommercebackend/EcommerceBackendApplication.java
@@ -0,0 +1,17 @@
+package com.training.ecommercebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceBackendApplication.class, args);
+	}
+
+}
+
+
+
+
